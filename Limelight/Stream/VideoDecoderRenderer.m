6800771cdce67a5bbda27c6d2e1b728b51034ef5
StringWeaver <55291103+StringWeaver@users.noreply.github.com>
2025-11-08 00:13:09 +0800

    performance optimize

--- Limelight/Stream/VideoDecoderRenderer.m
+++ Limelight/Stream/VideoDecoderRenderer.m
@@ -51,7 +51,6 @@ extern int ff_isom_write_av1c(AVIOContext *pb, const uint8_t *buf, int size,
     
    
     displayLayer.opaque = YES;
-    displayLayer.drawsAsynchronously = YES;
     displayLayer.position = CGPointMake(CGRectGetMidX(_view.bounds), CGRectGetMidY(_view.bounds));
     displayLayer.bounds = _view.bounds;
     displayLayer.videoGravity = AVLayerVideoGravityResizeAspect;
